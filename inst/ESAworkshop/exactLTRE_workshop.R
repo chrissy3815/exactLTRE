a4840c5378d4be49384c541a062a1ae8437786bf
Chrissy Hernandez <cmh352@cornell.edu>
2022-08-13 15:42:11 -0400

    workshop materials

--- inst/ESAworkshop/exactLTRE_workshop.R
+++ inst/ESAworkshop/exactLTRE_workshop.R
@@ -29,20 +29,34 @@ A3<- matrix(data=c(0,0.4,0, 0,0,0.6, 6,0,0.25), nrow=3, ncol=3)
 # Approximate LTRE:
 # contributions to the difference in lambda
 cont_diff<- approximateLTRE(list(A1,A2), method='fixed')
+cont_diff; # matrix of contributions 
+sum(cont_diff); # sum of approximate contributions 
+lamDiff(list(A1,A2)); # true difference in lambda 
+
 # contributions to the variance of lambda
 cont_var<- approximateLTRE(list(A1,A2,A3), method='random')
+round(cont_var,digits=5); # matrix of contributions 
+sum(cont_var); # sum of contributions 
+lamVar(list(A1,A2,A3)); # true variance in lambda 
 
 # Exact LTRE:
 # contributions to the difference in lambda
 cont_diff<- exactLTRE(list(A1,A2), method='fixed')
+cbind(as.character(cont_diff$varying.indices.list),round(cont_diff$epsilons,digits=5))
+sum(cont_diff$epsilons); # sum of contributions 
+lamDiff(list(A1,A2)); # true difference in lambda 
+
 # contributions to the variance of lambda
 cont_var<- exactLTRE(list(A1,A2,A3), method='random')
+cbind(as.character(cont_var$varying.indices.list),round(cont_var$epsilons,digits=5)); 
+sum(cont_var$epsilons); # sum of contributions 
+lamVar(list(A1,A2,A3)); # true variance in lambda 
 
 # Generation Time:
 F1<- matrix(0, nrow=3, ncol=3)
 F1[1,3]<- A1[1,3]
 #F1 is all zeros, except the upper right corner which matches A1 for adult fertility
-gen_time<- generation_time(A1, F1)
+gen_time <- generation_time(A1, F1)
 
 # R0, the expected lifetime reproductive output for an individual
 R0<- r_nought(A1, F1)
@@ -63,8 +77,16 @@ eta_1<- lifespan(U1, all_ages=FALSE) # eta_1 should match the first entry of eta
 comadre<- cdb_fetch("comadre")
 compadre<- cdb_fetch("compadre")
 
+## Note: if you can't install Rcompadre because of OS incompatibility, then 
+# uncomment this line, make sure the file path is correct, and run it to load 
+# the matrices and metadata for the following few examples.
+# This file contains the following four objects: 
+# geocrinia, geocrinia_mats, spermophilus, spermophilus_mats,
+load('example_matrices.Rdata')
+
 # We'll first work through a simple example of fixed design LTREs with white-bellied frogs.
-geocrinia<- comadre[comadre$SpeciesAuthor=="Geocrinia_alba",]
+geocrinia<- cdb_flatten(comadre[comadre$SpeciesAuthor=="Geocrinia_alba",])
+# cdb_flatten extracts the metadata only, without the `mat` list object.
 
 # You can look through the metadata about these matrices in the object "geocrinia"
 # For a full list of the available metadata, try:
@@ -74,31 +96,26 @@ names(geocrinia)
 
 # Here's an example of pulling out some metadata:
 geocrinia[,c("SpeciesAccepted", "OrganismType", "Country", "MatrixPopulation", "MatrixTreatment")]
-# When you print metadata from comadre and compadre, you'll get a warning about
-# the 'mat' object, but you don't have to worry about it. It will probably look
-# like this:
-## Warning message:
-##   In geocrinia[, c("SpeciesAccepted", "OrganismType", "Country", "MatrixPopulation", :
-##                      'mat' was included in the output by default, although not selected
 
 # pull out the two matrices of interest:
-Aobj<- c(matA(comadre[comadre$MatrixID==248239,]),matA(comadre[comadre$MatrixID==248238, ]))
-lapply(Aobj, eigen, only.values=TRUE)
+geocrinia_mats<- c(matA(comadre[comadre$MatrixID==248239,]),matA(comadre[comadre$MatrixID==248238, ]))
+lapply(geocrinia_mats, eigen, only.values=TRUE)
 
 # Evaluate a fixed symmetric design LTRE:
-result<- exactLTRE(Aobj, method='fixed', fixed.directional = FALSE)
+result<- exactLTRE(geocrinia_mats, method='fixed', fixed.directional = FALSE)
 result$varying.indices.list
 barplot(t(result$epsilons[2:length(result$epsilons)]),
         names.arg=c("sJ", "f","sJ, f", "sA", "sJ, sA", "f, sA", "sJ, f, sA"), las=2)
 
 ## Spermophilus armatus, ground squirrels.
-spermophilus<- comadre[comadre$MatrixID %in% c(249840,249844),]
+spermophilus<- cdb_flatten(comadre[comadre$MatrixID %in% c(249840,249844),])
+# cdb_flatten extracts the metadata only.
 
 # Should we do a symmetric or directional LTRE?
 spermophilus[,c("MatrixID", "MatrixPopulation", "MatrixTreatment")]
 
 # pull out the matrices:
-spermophilus_mats<- c(matA(spermophilus[spermophilus$MatrixID==249840]), matA(spermophilus[spermophilus$MatrixID==249844]))
+spermophilus_mats<- c(matA(comadre[comadre$MatrixID==249840]), matA(comadre[comadre$MatrixID==249844]))
 
 # Maybe try running it both ways, and see how the interpretation changes.
 spermophilus_symm<- exactLTRE(spermophilus_mats, method='fixed', maxint=3, fixed.directional = FALSE)
